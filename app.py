32ebf12cc28e0dd06f051fa9937ffcbef2fd64e2
RyanRadicall <ryan.vicentew@gmail.com>
2026-05-26 09:25:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
+import calendar
 from datetime import date, datetime
 from supabase import create_client, Client
 from market import get_cotacoes
