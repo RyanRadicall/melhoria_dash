ed9d60ceb8bc273569c31a4a4bdcc7a85245d772
RyanRadicall <ryan.vicentew@gmail.com>
2026-05-25 22:45:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import date, datetime
 from supabase import create_client, Client
 from market import get_cotacoes
-from export import exportar_excel
+from export import gerar_excel
 from styles.main_css import apply_styles
 
 # ── Page config ───────────────────────────────────────────────────────────────
