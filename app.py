c82b0fff64292c04785772304ba4fe2556723f27
RyanRadicall <ryan.vicentew@gmail.com>
2026-05-26 10:14:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -193,6 +193,7 @@ def tela_login():
         st.markdown("<br>", unsafe_allow_html=True)
         email = st.text_input("E-mail", placeholder="seuemail@exemplo.com", key="auth_email")
         senha = st.text_input("Senha", type="password", placeholder="••••••••", key="auth_senha")
+        
         nome = ""
 
 if aba == "Criar conta":
